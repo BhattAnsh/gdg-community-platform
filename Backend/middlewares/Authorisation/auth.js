4e5a35b01d4093883c2a3fb9a5d2706eb9a49747
BhattAnsh <anshbhatt140@gmail.com>
2024-10-07 22:00:31 +0530

    added authorization file

---
+++ Backend/middlewares/Authorisation/auth.js
@@ -0,0 +1,24 @@
+import User from '../models/user.model.js';
+export const auth = async (req, res, next) => {
+    try {
+        const userId = req.params.userId;
+        const blogId = req.params.quizId;
+        const user = await User.findById(userId);
+        const blog = user.quizzes.id(blogId);
+        if(blog.createdBy != userId){
+            return res.status(401).json({
+                status: 'error',
+                message: 'Unauthorized',
+            });
+        }
+        next();
+
+
+
+    } catch (error) {
+        return res.status(401).json({
+        status: 'error',
+        message: 'Unauthorized',
+        });
+    }
+}
