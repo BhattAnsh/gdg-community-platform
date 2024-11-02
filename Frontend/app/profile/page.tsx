f7c4bfa5177ad30db8a60c44d7ae339ac7d56202
BhattAnsh <anshbhatt140@gmail.com>
2024-11-02 14:56:02 +0530

    created frontend for profile page

--- Frontend/app/profile/page.tsx
+++ Frontend/app/profile/page.tsx
@@ -1,19 +1,154 @@
-import React from 'react';
-import PersonalDetails from '@/components/personalDetails';
-import CodingDetails from '@/components/CodingDetails';
-function pages() {
-  return (
-  <>
-  <div className='bg-[#EFEFEF] flex items-center justify-center p-10 h-full'>
-    <div className='bg-[white] w-[100vw] p-10 flex flex-col items-center justify-center rounded-3xl gap-8'>   
-      <PersonalDetails /> 
-      <CodingDetails />
-      <div></div>
-      <div></div>
-    </div>
-  </div>
-  </>
-  )
-}
+"use client"
+// import React from "react";
+ import PersonalDetails from "@/components/personalDetails";
+ import CodingDetails from "@/components/CodingDetails";
+ import { Collection } from "@/components/Collection";
+ import { Blogs } from "@/components/blogs";
+// function pages() {
+//   return (
+//     <>
+//       <div className="bg-[#EFEFEF] flex items-center justify-center p-10 h-full">
+//         <div className="bg-[white] w-full p-10 flex flex-col items-center justify-center rounded-3xl gap-8">
+//           <PersonalDetails />
+//           <CodingDetails />
+//           <Collection />
+//           <Blogs />
+//           <div></div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
-export default pages;
+// export default pages;
+
+import React, { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+} from "@tabler/icons-react";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+export default function Page() {
+  const links = [
+    {
+      label: "Dashboard",
+      href: "#",
+      icon: (
+        <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Profile",
+      href: "#",
+      icon: (
+        <IconUserBolt className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Settings",
+      href: "#",
+      icon: (
+        <IconSettings className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+    {
+      label: "Logout",
+      href: "#",
+      icon: (
+        <IconArrowLeft className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
+      ),
+    },
+  ];
+  const [open, setOpen] = useState(false);
+  return (
+    <div
+      className={cn(
+        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 max-w-full mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+      )}
+    >
+      <div className="h-screen flex bg-[#EFEFEF] items-center justify-center ">
+        <Sidebar open={open} setOpen={setOpen}>
+          <SidebarBody className="justify-between gap-10">
+            <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+              {open ? <Logo /> : <LogoIcon />}
+              <div className="mt-8 flex flex-col gap-2">
+                {links.map((link, idx) => (
+                  <SidebarLink key={idx} link={link} />
+                ))}
+              </div>
+            </div>
+            <div>
+              <SidebarLink
+                link={{
+                  label: "Manu Arora",
+                  href: "#",
+                  icon: (
+                    <Image
+                      src="https://assets.aceternity.com/manu.png"
+                      className="h-7 w-7 flex-shrink-0 rounded-full"
+                      width={50}
+                      height={50}
+                      alt="Avatar"
+                    />
+                  ),
+                }}
+              />
+            </div>
+          </SidebarBody>
+        </Sidebar>
+      </div>
+      <Dashboard />
+    </div>
+  );
+}
+export const Logo = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        Acet Labs
+      </motion.span>
+    </Link>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <Link
+      href="#"
+      className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20"
+    >
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    </Link>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="bg-[#EFEFEF] flex items-start justify-center p-2 h-screen overflow-scroll">
+        <div className="bg-[white] w-full p-10 flex flex-col items-center justify-center rounded-2xl gap-8">
+          <PersonalDetails />
+          <CodingDetails />
+          <Collection />
+          <Blogs />
+          <div></div>
+        </div>
+      </div>
+    </>
+  );
+};
